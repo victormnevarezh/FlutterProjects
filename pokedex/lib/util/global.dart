55a4d52a8c31fd0e0cc633f49ffbf40a65f0033c
Víctor Manuel Nevárez Herrera <victor.m.nevarez.h@gmail.com>
2022-05-19 18:14:42 -0500

    Changes pokedex

--- pokedex/lib/util/global.dart
+++ pokedex/lib/util/global.dart
@@ -1,21 +1,7 @@
-import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 class Global {
   static const Color colorWhite = const Color(0xFFFFFFFF);
   static const Color colorPokedex = const Color(0xFFd03056);
   static const Color colorBlack = const Color(0xFF000000);
-
-  static void mensaje(BuildContext context, String mensaje, String titulo, {Color backGroundColorCustom = Colors.redAccent}) {
-
-    Flushbar(
-      title: titulo,
-      message: mensaje,
-      duration: const Duration(seconds: 3),
-      isDismissible: false,
-      flushbarPosition: FlushbarPosition.TOP,
-      flushbarStyle: FlushbarStyle.GROUNDED,
-      backgroundColor: backGroundColorCustom,
-    ).show(context);
-  }
 }
