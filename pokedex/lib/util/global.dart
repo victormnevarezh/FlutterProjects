e11de9fa20be67812b82349cdb32350c17c8b389
Víctor Manuel Nevárez Herrera <victor.m.nevarez.h@gmail.com>
2022-06-12 18:40:21 -0500

    Delete pokedex

--- pokedex/lib/util/global.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Global {
-  static const Color colorWhite = const Color(0xFFFFFFFF);
-  static const Color colorPokedex = const Color(0xFFd03056);
-  static const Color colorBlack = const Color(0xFF000000);
-}
